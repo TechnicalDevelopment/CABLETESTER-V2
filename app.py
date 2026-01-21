7b7ef4260723d0276db3b9c1e1e4fbd80902f04b
TechnicalDevelopment <84158123+TechnicalDevelopment@users.noreply.github.com>
2026-01-21 23:57:45 +0000

    update app.py

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 import os
 import sys
 from PyQt6.QtWidgets import QApplication
+from PyQt6.QtCore import Qt, QCoreApplication
 from ui.main_window import MainWindow
 
 # Forceer output naar SPI LCD (fb1)
 os.environ.setdefault("QT_QPA_PLATFORM", "linuxfb:fb=/dev/fb1")
 os.environ.setdefault("QT_QPA_FB", "/dev/fb1")
+
+QCoreApplication.setAttribute(Qt.ApplicationAttribute.AA_SynthesizeMouseForUnhandledTouchEvents, True)
+QCoreApplication.setAttribute(Qt.ApplicationAttribute.AA_SynthesizeTouchForUnhandledMouseEvents, True)
+
 
 def main():
     app = QApplication(sys.argv)
