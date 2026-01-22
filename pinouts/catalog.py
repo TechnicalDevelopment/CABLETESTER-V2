10177557044f8c6f6930a51448a1cbe3c37ed949
TechnicalDevelopment <84158123+TechnicalDevelopment@users.noreply.github.com>
2026-01-22 17:08:24 +0000

    lk37

--- pinouts/catalog.py
+++ pinouts/catalog.py
@@ -10,4 +10,5 @@ CATALOG = [
     Pinout("rj45", "RJ45 (8P8C)", [str(i) for i in range(1, 9)]),
     Pinout("xlr3", "XLR 3 (Audio / DMX)", ["1", "2", "3"]),
     Pinout("xlr5", "XLR 5 (DMX)", ["1", "2", "3", "4", "5"]),
+    Pinout("LK37", "LK37", [str(i) for i in range(1, 37)]),
 ]
